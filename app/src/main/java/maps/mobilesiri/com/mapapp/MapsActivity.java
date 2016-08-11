1093aebe7967bcf7bd152fe7967e81053c70c45e
samannaqvi <naqvi.saman93@gmail.com>
2016-08-11 22:14:05 +0500

    ..

--- app/src/main/java/maps/mobilesiri/com/mapapp/MapsActivity.java
+++ app/src/main/java/maps/mobilesiri/com/mapapp/MapsActivity.java
@@ -26,6 +26,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     /**
+     * 
      *
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
